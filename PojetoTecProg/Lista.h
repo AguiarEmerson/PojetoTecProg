f2de8546312bb63a5711dc41845f7d73241e62ad
kenla <kenla@DESKTOP-QSSHHFQ>
2023-05-22 13:13:33 -0300

    mudei

--- PojetoTecProg/Lista.h
+++ PojetoTecProg/Lista.h
@@ -1,20 +1,132 @@
 #pragma once
-#include "Elemento.h"
-template <class TL> class Lista
+//apagar
+#include<stdio.h>
+
+namespace Listas
 {
-private:
-	Elemento<TL>* pPrimeiro;
-	Elemento<TL>* pUltimo;
-	int tam;
-public:
-	Lista();
-	~Lista();
-	int getTam();
-	TL* getElem(int posicao);
-	void push(TL* item);
-	void pop(TL item);
-	Elemento<TL>* getPrim();
+	template <class TL>
+	class Lista
+	{
+	public:
+		Lista();
+		~Lista();
+		template <class TL> class Elemento
+		{
+		private:
+			Elemento<TL>* pProx;
+			TL* pInfo;
+		public:
+			Elemento() { pProx = nullptr; pInfo = nullptr; }
+			~Elemento() {}
+			TL* getData() { return pInfo; }
+			void setData(TL* pp) { pInfo = pp; }
+			void setProx(Elemento<TL>* prox) { pProx = prox; }
+			Elemento<TL>* goProx(Elemento<TL>* temp) { return temp->getProx(); }
+			Elemento<TL>* getProx() { return pProx; }
+
+		};
 
 
-};
+		int getTam();
+		TL* getElem(int posicao);
+		void push(TL* item);
+		void pop(TL* item);
+		Elemento<TL>* getPrim() { return pPrimeiro; }
 
+
+
+	private:
+		Elemento<TL>* pPrimeiro;
+		Elemento<TL>* pUltimo;
+		int tam;
+	};
+	template<class TL>
+	Lista<TL>::Lista()
+	{
+		pPrimeiro = nullptr;
+		pUltimo = 0;
+		tam = 0;
+	}
+	template<class TL>
+	Lista<TL>::~Lista()
+	{
+
+	}
+
+	template<class TL>
+	int Lista<TL>::getTam()
+	{
+		return tam;
+	}
+	template<class TL>
+	TL* Lista<TL>::getElem(int posicao) {
+		int i;
+		Elemento<TL>* temp = pPrimeiro;
+		for (i = 0; i < posicao; i++) {
+			temp = temp->goProx(temp);
+		}
+		return temp->getData();
+	}
+	template<class TL>
+	void Lista<TL> ::push(TL* item)
+	{
+		if (item)
+		{
+			if (pPrimeiro == nullptr)
+			{
+				Elemento<TL>* aux = new Elemento<TL>;
+				if (aux != nullptr) {
+					aux->setData(item);
+					pPrimeiro = aux;
+					pUltimo = aux;
+					tam++;
+				}
+
+
+			}
+			else
+			{
+				Elemento<TL>* temp = new Elemento<TL>;
+				temp->setData(item);
+				if (temp != nullptr)
+				{
+					pUltimo->setProx(temp);
+					pUltimo = temp;
+					tam++;
+				}
+
+			}
+
+
+		}
+
+	}
+	template<class TL>
+	void Lista<TL>::pop(TL* item)
+	{
+		Elemento<TL>* temp = pPrimeiro;
+		Elemento<TL>* antTemp = nullptr;
+		while (temp->getData() != item)
+		{
+			antTemp = temp;
+			temp = temp->goProx(temp);
+		}
+		if (temp == pPrimeiro)
+		{
+			pPrimeiro = temp->getProx();
+		}
+		else if (temp == pUltimo)
+		{
+			antTemp->setProx(nullptr);
+			pUltimo = antTemp;
+		}
+		else
+		{
+			antTemp->setProx(temp->getProx());
+		}
+		delete temp;
+		tam--;
+	}
+	//template<class TL>
+	//Lista<TL>::Elemento<TL>* Lista<TL>::getPrim() 
+}
--- PojetoTecProg/ListaEntidades.h
+++ PojetoTecProg/ListaEntidades.h
@@ -1,14 +1,18 @@
 #pragma once
 #include "Lista.h"
 #include "Entidade.h"
-class ListaEntidades
-{
-private:
-	Lista<Entidade> LEs;
-public:
-	ListaEntidades();
-	~ListaEntidades();
-	void incluir(Entidade* pE);
-	void percorrer();
-};
 
+namespace Listas 
+{
+	class ListaEntidades
+	{
+	private:
+		Lista<Entidade> LEs;
+	public:
+		ListaEntidades();
+		~ListaEntidades();
+		void incluir(Entidade* pE);
+		void percorrer();
+		void retirar(Entidade* pE);
+	};
+}
